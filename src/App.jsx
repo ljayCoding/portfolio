2af85c2efde42461210f9c2ce11b879061f5a746
lJayCoding <lukewebber392@gmail.com>
2023-05-24 08:57:40 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './app.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import Project from './components/Project/Project';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/*" element={<NotFoundPage />}></Route>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </Router>
     </div>
